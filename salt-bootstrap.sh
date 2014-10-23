2ad57d869a563f9f2a0928dc448666737b4d33db
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-10-23 19:18:24 +0200

    using salt-states of repo

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CURRENT=`pwd`
+BASENAME=`basename $CURRENT`
+
 python -c 'import urllib; print urllib.urlopen("https://bootstrap.saltstack.com").read()' > install_salt.sh
 
 read -p "Start Installation? (y/n) " -n 1
@@ -10,6 +13,7 @@ fi
 
 sudo sh install_salt.sh
 sudo sh -c "echo 'file_client: local' >> /etc/salt/minion"
+sudo ln -sf $CURRENT/salt /srv/salt
 sudo salt-call state.highstate
 
 exit 0
