68bb327841a8cb6a3a35bb4f831600141ae8569d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-10-23 19:04:17 +0200

    install minion only

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -1,4 +1,15 @@
-#!/bin/sh -
+#!/bin/bash
 
 python -c 'import urllib; print urllib.urlopen("https://bootstrap.saltstack.com").read()' > install_salt.sh
+
+read -p "Start Installation? (y/n) " -n 1
+echo
+if [ $REPLY != 'y' ]; then
+    exit 1      
+fi
+
 sudo sh install_salt.sh
+sudo sh -c "echo 'file_client: local' >> /etc/salt/minion"
+sudo salt-call state.highstate
+
+exit 0
