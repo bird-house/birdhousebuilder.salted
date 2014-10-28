392e18bd3d780ac81ae1ed6de099e62e6f25d376
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-10-28 12:40:18 +0100

    using bootstrap script from saltstack github fork

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -3,7 +3,10 @@
 CURRENT=`pwd`
 BASENAME=`basename $CURRENT`
 
-python -c 'import urllib; print urllib.urlopen("https://bootstrap.saltstack.com").read()' > install_salt.sh
+#SCRIPT_URL="https://bootstrap.saltstack.com"
+SCRIPT_URL="https://raw.githubusercontent.com/cehbrecht/salt-bootstrap/develop/bootstrap-salt.sh"
+
+python -c "import urllib; print urllib.urlopen(\"$SCRIPT_URL\").read()" > install_salt.sh
 
 read -p "Start Installation? (y/n) " -n 1
 echo
@@ -11,7 +14,7 @@ if [ $REPLY != 'y' ]; then
     exit 1      
 fi
 
-sudo sh install_salt.sh
+sudo sh install_salt.sh stable
 sudo sh -c "echo 'file_client: local' >> /etc/salt/minion"
 sudo ln -sf $CURRENT/salt /srv/salt
 #sudo salt-call state.highstate
