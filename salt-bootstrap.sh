d5ab11d80af63fed3ebd5abd934b712374157f7a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-10-28 17:50:04 +0100

    fixed salt link, check if install script exists

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -8,6 +8,10 @@ SCRIPT_URL="https://raw.githubusercontent.com/cehbrecht/salt-bootstrap/develop/b
 
 python -c "import urllib; print urllib.urlopen(\"$SCRIPT_URL\").read()" > install_salt.sh
 
+if [ ! -f "install_salt.sh" ]; then
+    exit 1
+fi
+
 read -p "Start Installation? (y/n) " -n 1
 echo
 if [ $REPLY != 'y' ]; then
@@ -16,7 +20,7 @@ fi
 
 sudo sh install_salt.sh stable
 sudo sh -c "echo 'file_client: local' >> /etc/salt/minion"
-sudo ln -sf $CURRENT/salt /srv/salt
+sudo ln -sf $CURRENT/salt /srv
 #sudo salt-call state.highstate
 
 exit 0
