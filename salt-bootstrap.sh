5ace065badf6cf143cde33d5a0981dc2908cd0ad
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-10-29 14:15:40 +0100

    comments added

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -10,18 +10,24 @@ SCRIPT_URL="https://raw.githubusercontent.com/saltstack/salt-bootstrap/develop/b
 python -c "import urllib; print urllib.urlopen(\"$SCRIPT_URL\").read()" > install_salt.sh
 
 if [ ! -f "install_salt.sh" ]; then
+    echo "Downloading bootstrap-salt.sh script failed."
     exit 1
 fi
 
-read -p "Start Installation? (y/n) " -n 1
+read -p "Start SaltStack Installation? (y/n) " -n 1
 echo
 if [ $REPLY != 'y' ]; then
     exit 1      
 fi
 
 sudo sh install_salt.sh stable
+
+echo "Setting up minon-only configuration ..."
+
 sudo sh -c "echo 'file_client: local' >> /etc/salt/minion"
 sudo ln -sf $CURRENT/salt /srv
 #sudo salt-call state.highstate
+
+echo "Installing SaltStack minion done."
 
 exit 0
